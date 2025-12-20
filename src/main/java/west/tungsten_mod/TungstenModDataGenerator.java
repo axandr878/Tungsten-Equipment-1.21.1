c7b330773b2551205980a3103490c81af5bf5634
axandr878 <axandr878@gmail.com>
2025-12-20 20:46:09 +1100

    first setup/commit

---
+++ src/main/java/west/tungsten_mod/TungstenModDataGenerator.java
@@ -0,0 +1,11 @@
+package west.tungsten_mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TungstenModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
