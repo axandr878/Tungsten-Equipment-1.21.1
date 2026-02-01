66cc3de4cd3bed5d5549b65b3c8b558303f95345
axandr878 <axandr878@gmail.com>
2026-02-01 21:46:41 +1100

    legacy texture pack added

--- src/main/java/west/tungsten_mod/TungstenMod.java
+++ src/main/java/west/tungsten_mod/TungstenMod.java
@@ -1,7 +1,11 @@
 package west.tungsten_mod;
 
 import net.fabricmc.api.ModInitializer;
-
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import west.tungsten_mod.block.ModBlocks;
@@ -20,5 +24,22 @@ public class TungstenMod implements ModInitializer {
 		ModItemGroups.registerItemGroups();
 		ModLootTables.register();
 		ModWorldGeneration.generateModWorldGen();
+
+		registerBuiltinResourcePack();
+	}
+
+	private static void registerBuiltinResourcePack() {
+		Identifier packId = Identifier.of(MOD_ID, "legacy");
+
+		ModContainer container = FabricLoader
+				.getInstance()
+				.getModContainer(MOD_ID)
+				.orElseThrow();
+
+		ResourceManagerHelper.registerBuiltinResourcePack(
+				packId,
+				container,
+				ResourcePackActivationType.NORMAL
+		);
 	}
 }
